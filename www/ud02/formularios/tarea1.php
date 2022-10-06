facf2b09c0aa697a1aca01406b76ef4a50c77a27
Sabela <sabela@iessanclemente.net>
2022-10-06 19:30:54 +0200

    reesturcturado

---
+++ www/ud02/formularios/tarea1.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**  Cree un formulario que solicite su nombre y apellido. Cuando se reciben los datos, se debe mostrar la siguiente información:
+ * Nombre: `xxxxxxxxx`
+ *  Apellidos: `xxxxxxxxx`
+ * Nombre y apellidos: `xxxxxxxxxxxx xxxxxxxxxxxx`
+ * Su nombre tiene caracteres `X`.
+ * Los 3 primeros caracteres de tu nombre son: `xxx`
+ * La letra A fue encontrada en sus apellidos en la posición: `X`
+ * Tu nombre en mayúsculas es: `XXXXXXXXX`
+ * Sus apellidos en minúsculas son: `xxxxxx`
+ * Su nombre y apellido en mayúsculas: `XXXXXX XXXXXXXXXX`
+ * Tu nombre escrito al revés es: `xxxxxx`
+*/
+
+
+
+
+?>
---
+++ www/ud02/formularios/tarea2_optativo.php
@@ -0,0 +1,29 @@
+<select name="opcion">
+<option value="cocacola">Coca Cola</option>
+<option value="pepsi">Pepsi Cola</option>
+<option value="fanta">Fanta Naranja</option>
+<option value="trina">Trina Manzana</option>
+</select>
+
+<?php 
+
+
+/*
+Crea un formulario para solicitar una de las siguientes bebidas:
+
+    Bebida|PVP
+    :-|:-:
+    Coca Cola|1 €
+    Pepsi Cola|0,80 €
+    Fanta Naranja|0,90 €
+    Trina Manzana|1,10 €
+    
+    A mayores, necesitamos un campo adicional con la cantidad de bebidas a comprar y un botón de <kbd>Solicitar</kbd>.
+    
+    La aplicación mostrará algo como:
+
+    Pediste 3 botellas de Coca Cola. Precio total a pagar: 3 Euros.
+    Puedes utilizar sentencias `switch` o `if`.
+    */
+
+?>
