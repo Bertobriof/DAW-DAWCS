1c8ba1fa832c700ddd5d616e1aae52bfd34695f2
sabela <sabela@infol00.sanclemente.local>
2023-01-16 14:26:01 +0100

    ejercicios ud05

---
+++ www/ud05/2.2.fecha.php
@@ -0,0 +1,21 @@
+
+<?php 
+
+class Data
+{
+ public static $calendario = "Calendario gregoriano";
+
+ public static function getData()
+ {
+ $ano = date('Y'); //Nos da el año actual 
+ $mes = date('m');
+ $dia = date('d');
+ return $dia . '/' . $mes . '/' . $ano;
+ }
+}
+
+/*
+- ```getCalendar()```: que devolverá el valor de esta propiedad. 
+- ```getHora()```: que devuelve la hora en el siguiente formato HH:MM:SS. 
+- ```getDataHora()```: que llamará a los métodos ```getData()``` y ```getHora()``` para mostrar tanto la fecha como la hora. 
+*/ 
