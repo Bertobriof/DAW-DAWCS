5867e56e7ddaaf642cc40f447794e4e8e295b33d
Sabela <sabela@iessanclemente.net>
2023-02-12 19:30:52 +0100

    und06

---
+++ www/ud06/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+/*  Tabla Clientes
+Se debe permitir las siguientes acciones sobre la tabla clientes y la ruta ```/clientes```: 
+- GET: Al acceder a esta ruta se debe mostar todos los datos de los clientes. 
+    Optativo. Mostrar la información de un único cliente a través del id.  
+- POST: Se debe poder insertar un cliente en la base de datos. 
+- DELETE: Dado un id se debe poder eliminar un cliente.
+- PUT: Se podrá modificar de un cliente sus apellidos, edad, email y teléfono. 
+*/ 
+
+/* Tabla Hoteles
+
+Se debe permitir las siguientes acciones sobre la tabla clientes y la ruta ```/hoteles```: 
+
+- GET: Al acceder a esta ruta se debe mostar todos los datos de los hoteles. 
+    Optativo. Mostrar la información de un único hotel a través del id.  
+- POST: Se debe poder insertar un hotel en la base de datos. 
+- DELETE: Dado un id se debe poder eliminar un hotel.
+- PUT: Se podrá modificar de un hotel sus direccion, email y teléfono. 
+*/
+/* Tabla Reserva
+
+Se debe permitir las siguientes acciones sobre la tabla clientes y la ruta ```/reservas```. Hay que tener en cuenta que esta tabla tiene dependencias con las otras dos tablas. 
+
+- GET: Al acceder a esta ruta se debe mostar todas las reservas realizadas por todos los clientes en todos los hoteles.  
+    Optativo. Mostrar la información de un única reserva a través del id.  
+- POST: Se debe poder insertar una reserva en la base de datos. Identificar los datos necesarios.  
+- DELETE: Dado un id se debe poder eliminar una reserva.
+- PUT: Se podrá modificar de una reserva la fecha de entrada y la fecha de salida.
+ */
